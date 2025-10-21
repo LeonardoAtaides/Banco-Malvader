a711698cf431363e972758934fd771dce24ae125
Joel Júnior Carvalho De Oliveira <164202659+JoeJr09@users.noreply.github.com>
2025-10-21 16:24:04 -0300

    att import index do server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { json } from 'body-parser';
 import cors from 'cors';
 import { createConnection } from './config/database';
-import routes from './routes/index';
+import routes from './types/index';
 import errorHandler from './middleware/errorHandler';
 
 const app = express();
