844861f337d2c02ad30dd2aa9e174019e4d43fa0
Ataídes <162513948+LeonardoAtaides@users.noreply.github.com>
2025-11-02 07:08:24 -0300

    Update page.tsx

--- frontend/src/app/Cliente/Investimentos/page.tsx
+++ frontend/src/app/Cliente/Investimentos/page.tsx
@@ -12,8 +12,6 @@ const InvitePage: React.FC = () => {
     router.back();
   };
 
-
-
   return (
     <main className="min-h-screen bg-gradient-to-b from-[#012E4B] to-[#064F75] text-white flex flex-col justify-between">
       {/* Conteúdo principal */}
@@ -32,52 +30,47 @@ const InvitePage: React.FC = () => {
         Meus Investimentos
       </h1>
 
-      <Titulo tipo={5} />
-      {/* Conta Poupança */}
-    <div className="mt-6 p-4 bg-white/10 rounded-xl border-1 border-white/40">
-    <div className="flex justify-between mb-2">
-        <div>
-        <p className="text-sm text-gray-300">Saldo Atual</p>
-        <p className="text-lg font-semibold">R$ 300,00</p>
+    {/* Conta Poupança */}
+    <Titulo tipo={5} />
+
+    <div className="mt-6 p-4 bg-white/10 rounded-xl border-1 border-white/40 mb-5">
+        <div className="flex justify-between mb-2">
+            <div>
+                <p className="text-sm text-gray-300">Saldo Atual</p>
+                <p className="text-lg font-semibold">R$ 300,00</p>
+            </div>
+            <div className="text-right">
+                <p className="text-sm text-gray-300">Rendimento</p>
+                <p className="text-[#42D23A] font-semibold">+ R$ 1,50</p>
+            </div>
         </div>
-        <div className="text-right">
-        <p className="text-sm text-gray-300">Rendimento</p>
-        <p className="text-[#42D23A] font-semibold">+ R$ 1,50</p>
+        <div className="text-sm text-gray-300 space-y-1">
+            <p>Taxa de rendimento: 0,5%</p>
+            <p>Abertura: 01/09/2025</p>
         </div>
-    </div>
-    <div className="text-sm text-gray-300 space-y-1">
-        <p>Taxa de rendimento: 0,5%</p>
-        <p>Abertura: 01/09/2025</p>
-    </div>
     </div>
 
+    {/* Conta Investimento */}
      <Titulo tipo={6} />
-
-     {/* Conta Investimento */}
+     
     <div className="mt-6 p-4 bg-white/10 rounded-xl border-1 border-white/40">
-    <div className="flex justify-between mb-2">
-        <div>
-        <p className="text-sm text-gray-300">Saldo Aplicado</p>
-        <p className="text-lg font-semibold">R$ 500,00</p>
+        <div className="flex justify-between mb-2">
+            <div>
+                <p className="text-sm text-gray-300">Saldo Aplicado</p>
+                <p className="text-lg font-semibold">R$ 500,00</p>
+            </div>
+            <div className="text-right">
+                <p className="text-sm text-gray-300">Rendimento</p>
+                <p className="text-[#42D23A] font-semibold">+ R$ 52,50</p>
+            </div>
         </div>
-        <div className="text-right">
-        <p className="text-sm text-gray-300">Rendimento</p>
-        <p className="text-[#42D23A] font-semibold">+ R$ 52,50</p>
+        <div className="text-sm text-gray-300 space-y-1">
+            <p>Taxa de base: 10,5%</p>
+            <p>Perfil de risco: Médio</p>
+            <p>Valor mínimo: R$ 200,00</p>
+            <p>Abertura: 01/10/2025</p>
         </div>
     </div>
-    <div className="text-sm text-gray-300 space-y-1">
-        <p>Taxa de base: 10,5%</p>
-        <p>Perfil de risco: Médio</p>
-        <p>Valor mínimo: R$ 200,00</p>
-        <p>Abertura: 01/10/2025</p>
-    </div>
-    </div>
-
-      
-        
-        
-
-
 
       </div>
     </main>
