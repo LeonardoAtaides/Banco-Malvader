c3e3f4a97fd77b2f4f91a936d1c0f202802b7f0b
Ataídes <162513948+LeonardoAtaides@users.noreply.github.com>
2025-11-01 23:49:18 -0300

    add page cliente

---
+++ frontend/src/app/Cliente/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+export default function Cliente() {
+ 
+  return (
+    <main className="bg-gradient-to-b from-[#012E4B] to-[#064F75] min-h-screen">
+        <h1 className="text-white text-2xl font-bold">Cliente</h1>
+    </main>
+  );
+}
