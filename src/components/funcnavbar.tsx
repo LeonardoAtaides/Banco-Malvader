e85b0da60d46542a7b238c2f7b1d67b393667a17
Ataídes <162513948+LeonardoAtaides@users.noreply.github.com>
2025-11-04 22:57:16 -0300

    Create navbar para funcionário

---
+++ src/components/funcnavbar.tsx
@@ -0,0 +1,25 @@
+import { Home, User, Menu } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export default function BottomNavFunc() {
+  const router = useRouter();
+
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 bg-[#012E4B] flex justify-around items-center py-3 text-white shadow-t z-50">
+      <button onClick={() => router.push("/Funcionario/Perfil")} className="flex flex-col items-center text-xs">
+        <User className="w-6 h-6 mb-1" />
+        Perfil
+      </button>
+
+      <button onClick={() => router.push("/Funcionario")} className="flex flex-col items-center text-xs">
+        <Home className="w-6 h-6 mb-1" />
+        Home
+      </button>
+
+      <button onClick={() => router.push("/Funcionario/Menu")} className="flex flex-col items-center text-xs">
+        <Menu className="w-6 h-6 mb-1" />
+        Menu
+      </button>
+    </nav>
+  );
+}
